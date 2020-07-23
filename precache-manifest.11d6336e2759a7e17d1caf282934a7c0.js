e294fcc5bdacb33ff5c94738ba3999cea3c23f67
rahul6373paul <rahul6373paul@github.com>
2020-07-23 13:28:16 +0530

    Updates

---
+++ precache-manifest.11d6336e2759a7e17d1caf282934a7c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6f96c52fe93509a0db0d943b5cfc9a",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "942dd76f35e531a83257",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "161b15d6f54cebdf3965",
+    "url": "/RoboFriends/static/css/main.e4798efa.chunk.css"
+  },
+  {
+    "revision": "942dd76f35e531a83257",
+    "url": "/RoboFriends/static/js/2.a71a67f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.a71a67f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161b15d6f54cebdf3965",
+    "url": "/RoboFriends/static/js/main.6e713c2f.chunk.js"
+  },
+  {
+    "revision": "09248c1cf26425cd4cba",
+    "url": "/RoboFriends/static/js/runtime-main.a57902e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be2d1d7601ab5b506539d41f7c0e8a56.js"
+  "/RoboFriends/precache-manifest.11d6336e2759a7e17d1caf282934a7c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboFriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
